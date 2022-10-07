10fdf1ae739a25d1c9cf17ff58b4204b9eaf1e09
alicenie <fnie9421@gmail.com>
2022-10-07 18:59:36 -0400

    add tooltip; minor changes

--- src/ui.tsx
+++ src/ui.tsx
@@ -4,23 +4,45 @@ import {
   render,
   TextboxMultiline,
   useInitialFocus,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h } from 'preact'
-import { useCallback, useState } from 'preact/hooks'
+  VerticalSpace,
+  Dropdown,
+  Text,
+  DropdownOption,
+} from "@create-figma-plugin/ui";
+import { emit } from "@create-figma-plugin/utilities";
+import { h } from "preact";
+import { useCallback, useState } from "preact/hooks";
 
 function Plugin(props: { text: string }) {
-  const [text, setText] = useState(props.text)
+  const [text, setText] = useState(props.text);
   const handleUpdateButtonClick = useCallback(
     function () {
-      emit('UPDATE_TEXT', text)
+      emit("UPDATE_TEXT", text);
     },
     [text]
-  )
+  );
+
+  const [value, setValue] = useState<null | string>(null);
+  const options: Array<DropdownOption> = [
+    { value: "foo" },
+    { value: "bar" },
+    { value: "baz" },
+    { separator: true },
+    { header: "Header" },
+    { value: "qux" },
+  ];
+  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
+    const newValue = event.currentTarget.value;
+    console.log(newValue);
+    setValue(newValue);
+  }
+
+  // if (text === "show Archived") {
   return (
     <Container space="medium">
       <VerticalSpace space="large" />
+      <Dropdown onChange={handleChange} options={options} value={value} />
+      <Text>{text}</Text>
       <TextboxMultiline
         {...useInitialFocus()}
         onValueInput={setText}
@@ -33,7 +55,30 @@ function Plugin(props: { text: string }) {
       </Button>
       <VerticalSpace space="small" />
     </Container>
-  )
+  );
+  // } else {
+  //   return (
+  //     <Container space="medium">
+  //       <VerticalSpace space="large" />
+  //       {/* <Dropdown onChange={handleChange} options={options} value={value} />
+  //       <Text>{text}</Text>
+  //       <TextboxMultiline
+  //         {...useInitialFocus()}
+  //         onValueInput={setText}
+  //         value={text}
+  //         variant="border"
+  //       /> */}
+  //       <VerticalSpace space="large" />
+  //       <Button fullWidth onClick={handleUpdateButtonClick}>
+  //         Cancel
+  //       </Button>
+  //       <Button fullWidth onClick={handleUpdateButtonClick}>
+  //         Delete
+  //       </Button>
+  //       <VerticalSpace space="small" />
+  //     </Container>
+  //   );
+  // }
 }
 
-export default render(Plugin)
+export default render(Plugin);
