8016a2bc222da25b6a738d4b7a2b415a5dcca470
alicenie <fnie9421@gmail.com>
2022-12-15 19:20:03 -0500

    add readme and gitignore

--- plugin/src/ui.tsx
+++ plugin/src/ui.tsx
@@ -1,80 +1,9 @@
-import {
-  Button,
-  Columns,
-  Container,
-  Muted,
-  render,
-  Text,
-  TextboxNumeric,
-  VerticalSpace,
-} from "@create-figma-plugin/ui";
-import { emit } from "@create-figma-plugin/utilities";
-import { h } from "preact";
-import { useCallback, useState } from "preact/hooks";
+import { Container, render } from "@create-figma-plugin/ui";
 
-import {
-  DisplayNodesHandler,
-  SaveNodesHandler,
-  ToggleWidget,
-  CreateWidget,
-} from "./types";
+import { h } from "preact";
 
 function Plugin() {
-  const [count, setCount] = useState<number | null>(5);
-  const [countString, setCountString] = useState("5");
-  const handleSaveNodesButtonClick = useCallback(
-    function () {
-      if (count !== null) {
-        emit<SaveNodesHandler>("SAVE_NODES");
-      }
-    },
-    [count]
-  );
-
-  const handleCloseButtonClick = useCallback(function () {
-    emit<DisplayNodesHandler>("DISPLAY_NODES");
-  }, []);
-
-  const handleToggleWidget = useCallback(function () {
-    emit<ToggleWidget>("TOGGLE_WIDGET");
-  }, []);
-
-  const handleCreateWidget = useCallback(function () {
-    emit<CreateWidget>("CREATE_WIDGET");
-  }, []);
-
-  return (
-    <Container space="medium">
-      <VerticalSpace space="large" />
-      {/* <Text>
-        <Muted>Count</Muted>
-      </Text>
-      <VerticalSpace space="small" />
-      <TextboxNumeric
-        onNumericValueInput={setCount}
-        onValueInput={setCountString}
-        value={countString}
-        variant="border"
-      /> */}
-      <VerticalSpace space="extraLarge" />
-      <Columns space="extraSmall">
-        <Button fullWidth onClick={handleSaveNodesButtonClick}>
-          Save
-        </Button>
-        <Button fullWidth onClick={handleCloseButtonClick} secondary>
-          Display
-        </Button>
-      </Columns>
-      <br></br>
-      <Button fullWidth onClick={handleToggleWidget} secondary>
-        Toggle Widget
-      </Button>
-      <Button fullWidth onClick={handleCreateWidget} secondary>
-        Create Widget
-      </Button>
-      <VerticalSpace space="small" />
-    </Container>
-  );
+  return <Container space="medium">placeholder</Container>;
 }
 
 export default render(Plugin);
